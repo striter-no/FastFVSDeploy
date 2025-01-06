43da9a527ddfb2a6d9ce435d1d9465e2582cf3f7
aserogl <aserogl31@gmail.com>
2025-01-06 13:14:48 +0000

    added generate sshd_config and hostkeys scripts

---
+++ configs/gen-ssh-config.sh
@@ -0,0 +1,29 @@
+usage() {
+    [ $# -ne 0 ] && echo "$*"
+    echo 'usage: gen-ssh-config.sh OUTPUT_DIRECTORY'
+}
+
+keys_dir="$(realpath "$1")"
+
+if [ $# -eq 0 ]; then
+    usage
+    exit 2
+fi
+
+if [ -f "$keys_dir" ]; then
+    usage "$keys_dir is exists and a file"
+    exit 2
+fi
+
+cat << EOF
+HostKey $keys_dir/ssh_host_rsa_key
+HostKey $keys_dir/ssh_host_ecdsa_key
+HostKey $keys_dir/ssh_host_ed25519_key
+ChallengeResponseAuthentication no
+UsePAM yes
+X11Forwarding yes
+PrintMotd no
+AcceptEnv LANG LC_*
+Subsystem       sftp    /usr/lib/openssh/sftp-server
+EOF
+
